af5e3e622667bb056832b07a61510fa5612d8697
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-04-16 20:49:13 +0400

    skeletons

---
+++ src/game.js
@@ -0,0 +1,11 @@
+let WIDTH = HEIGHT = 800;
+
+function setup() {
+    createCanvas(WIDTH, HEIGHT);
+    background(230);
+
+}
+
+function draw() {
+
+}
---
+++ src/player.js
@@ -0,0 +1,14 @@
+class Player{
+    constructor(){
+        this.x = 50;
+        this.y = HEIGHT - 50;
+
+    }
+
+    Show(){
+        
+    }
+    update(){
+
+    }
+}
