1b4247c86f05b7425ca5ac27e755e03e792f92e8
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-04-17 17:42:18 +0400

    fixed ground constructor

--- src/ground.js
+++ src/ground.js
@@ -3,11 +3,11 @@ class Ground {
      * 
      * @param {number} x X Coord of top left of ground
      * @param {number} y y Coord of top left of ground
-     * @param {number} width width of the ground
+     * @param {number} w width of the ground
      */
-    constructor(x, y) {
-        this.pos = createVector(x, y, width);//top left position
-        this.width = width;
+    constructor(x, y, w) {
+        this.pos = createVector(x, y);//top left position
+        this.width = w;
     }
 
     Show() {
@@ -21,7 +21,6 @@ class Ground {
         //move to the left at x speed
         this.pos.x -= groundSpeed;
     }
-
 
     Move() {
 
