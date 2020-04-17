65e3d636b202d33c5c3104be27f793e06d849362
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-04-17 11:46:42 +0400

    began adding constants

--- src/game.js
+++ src/game.js
@@ -1,4 +1,10 @@
 let WIDTH = HEIGHT = 800;
+const gravity = 10; //downward acceleration
+
+//PLAYER SETTINGS
+const player_x = 50;
+const player_y = HEIGHT-50;
+const player_r = 10;
 
 function setup() {
     createCanvas(WIDTH, HEIGHT);
@@ -7,5 +13,5 @@ function setup() {
 }
 
 function draw() {
-
+    
 }
