a5cd0bb3267cb2f8fe27db7b3b54b9c1370ae470
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-04-17 11:46:27 +0400

    added player skeleton

--- src/player.js
+++ src/player.js
@@ -1,14 +1,39 @@
-class Player{
-    constructor(){
-        this.x = 50;
-        this.y = HEIGHT - 50;
+class Player {
+    constructor(genome) {
+        this.x = player_x;
+        this.y = player_y;
+        this.r = player_r;
+
+        this.fitness = 0;
+        this.dead = false;
+
+        if (genome) {
+            this.brain = genome;
+        } else {
+            this.brain = new Genome(4, 1);
+        }
 
     }
 
-    Show(){
-        
-    }
-    update(){
+    //draw the player to canvas
+    Show() {
+        if (this.dead == false) {
+            //we want two ellipses, one on top and one on bottom, with some lines connecting them to make a player shape
 
+        }
+    }
+
+    //updates the physics of this player, checking for collisions, and checking for death
+    Update() {
+        if (this.dead == false){
+
+        }
+    }
+
+    //compute the genomes move, and make that move (jump or no jump) based on inputs
+    Move() {
+        if (this.dead == false){
+
+        }
     }
 }
