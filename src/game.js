fe0ed3ec521c282ca800d2be103c6d46a629b2f5
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-04-17 12:34:45 +0400

    changed player settings

--- src/game.js
+++ src/game.js
@@ -2,16 +2,23 @@ let WIDTH = HEIGHT = 800;
 const gravity = 10; //downward acceleration
 
 //PLAYER SETTINGS
-const player_x = 50;
-const player_y = HEIGHT-50;
-const player_r = 10;
+const player_x = 100;
+const player_y = HEIGHT - 400;
+const player_r = 20;
+
+let p;
+
+//neat
+let best_fitness = 1;
 
 function setup() {
     createCanvas(WIDTH, HEIGHT);
     background(230);
 
+    p = new Player();
 }
 
 function draw() {
-    
+    background(230);
+    p.Show();
 }
