0b0ab7051dd01e70e22741be973bce4d222d7622
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-04-17 17:42:29 +0400

    game works

--- src/game.js
+++ src/game.js
@@ -1,14 +1,22 @@
 let WIDTH = HEIGHT = 800;
-const gravity = 1; //downward acceleration
+const gravity = 0.5; //downward acceleration
 
 //PLAYER SETTINGS
-const player_x = 100;
+const player_x = 50;
 const player_y = HEIGHT - 400;
 const player_r = 20;
 
-const jumpStrength = 20;
+const jumpStrength = 40;
+
+
+//ground settings
+const groundSpeed = 10; //(x speed);
+const groundHeight = 20; //how tall each platform should be
 
 let p;
+
+//array of ground pieces
+let grounds = [];
 
 //neat
 let best_fitness = 1;
@@ -18,6 +26,19 @@ function setup() {
     background(230);
 
     p = new Player();
+
+
+    let x = 0;
+    let lasty = 600;//start block
+    let y = lasty;
+    for (let i = 0; i < 5; i++) {
+        grounds.push(new Ground(x, y, 600));
+        y = random(lasty - 200, lasty + 200);
+        if (y < 100) y = random(lasty, lasty + 200);
+        if (y > HEIGHT - 100) y = random(lasty, lasty - 200);
+        lasty = y;
+        x += WIDTH;
+    }
 }
 
 function draw() {
@@ -26,9 +47,12 @@ function draw() {
     p.Update();
     //p.Move();
 
+    grounds.forEach(g => {
+        g.Show();
+        g.Update();
+    })
+
     noStroke();
-    fill(50, 155, 0);//ground
-    rect(0, HEIGHT - 100, WIDTH, 100);
 }
 
 function keyPressed() {
